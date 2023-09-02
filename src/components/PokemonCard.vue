8b4fd97b384ce58d52cd5ae1f426d70c07fa8f69
Carlos Rodrigues <crodrigsantos@gmail.com>
2023-09-02 12:12:40 -0300

    feat: ajuste na pesquisa
    Converter a busca por pokémon para caracter minúsculo

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -75,7 +75,7 @@ export default defineComponent({
     };
 
     const searchPokemon = (search: string) => {
-      getPokemon(search);
+      getPokemon(search.toLowerCase());
     };
 
     const previousPokemon = () => {
